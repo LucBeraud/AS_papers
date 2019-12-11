4038b06c3f2d701432366463b66d82dbd11e63ab
LucBeraud <beraud.luc@laposte.net>
2019-12-11 11:26:27 +0100

    mise a jour du code

--- test.py
+++ test.py
@@ -11,12 +11,20 @@ lines = []
 
 with open('ISPRS2020_auteurs.csv','r',encoding='latin-1') as file:
     for line in file.readlines():
-        line=np.array(line.split(';'))
+        line_tmp=np.array(line.split('"'))
+        if len(line_tmp)==0:
+            line_tmp = line
+        line=[]
+        for txt in line_tmp:    
+#            line.append(np.array(txt.split(';')))
+            line.append(txt.split(';'))
         lines.append(line)
-#        print(lines[-1].shape)
+        print(len(lines[-1]))
 print('Data loaded !\n')
 print('Attention !!! la colonne paper_authors contient parfois des gens séparés par ";" ou "," -> fuck up le nb de colonnes lues.')
 #lines=np.array(lines)
 #for i in range(len(lines[1])):
 #    print(lines[0][i],lines[1][i])
 #    print('-----------------')
+#print (lines[1])
+print(lines[0])
