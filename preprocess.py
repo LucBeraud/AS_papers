0e12290141cc2bd67b6402852e7bed85ed4191d4
LucBeraud <beraud.luc@laposte.net>
2020-01-15 10:57:52 +0100

    code python preprocessing - debut psycopg2 connecter a une bdd - update

--- preprocess.py
+++ preprocess.py
@@ -7,15 +7,19 @@ Created on Wed Jan 15 10:44:07 2020
 
 import psycopg2
 
-try:
-    conn = psycopg2.connect(dbname="test_isprs", user="postgres", password="postgres",host="localhost",port="5433")
-except:
-    print("I am unable to connect to the database")
+def preprocessing():
+    try:
+        conn = psycopg2.connect(dbname="test_isprs", user="postgres", password="postgres",host="localhost",port="5433")
+    except:
+        print("I am unable to connect to the database")
+    
+    cur = conn.cursor()
+    try:
+        cur.execute("SELECT * FROM country")
+        rows = cur.fetchall()
+        print(rows)
+    except psycopg2.Error as e:
+        pass
 
-cur = conn.cursor()
-try:
-    cur.execute("SELECT * FROM country")
-    rows = cur.fetchall()
-    print(rows)
-except psycopg2.Error as e:
-    pass
+if __name__ == '__main__':
+    preprocessing()
